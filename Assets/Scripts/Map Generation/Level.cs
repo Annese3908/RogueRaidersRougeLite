61eadc7c7a5cb130ceae3dd441d9a1cc0f56b73d
Annese3908 <annese240@gmail.com>
2025-03-09 18:14:16 -0700

    Asset Inclusion

---
+++ Assets/Scripts/Map Generation/Level.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Level
+{
+   public static float _HEIGHT = 500;
+   public static float _WIDTH = 500;
+
+   public static float _SCALE = 1.0f;
+   public static Sprite _BossRoomIcon;
+   public static Sprite _SpawnRoomIcon;
+   public static Sprite _UnexploredRoomIcon;
+   public static Sprite _CurrentRoomIcon;
+   public static Sprite _PlantRoomIcon;
+   public static Sprite _DefaultRoomIcon;
+
+
+
+}
