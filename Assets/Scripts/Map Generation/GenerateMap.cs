648c271e96b6843c8f74ddc37e27626218cf73e1
Annese3908 <008493908@coyote.csusb.edu>
2025-03-10 16:46:13 -0700

    Map Progression and File Organization

--- Assets/Scripts/Map Generation/GenerateMap.cs
+++ Assets/Scripts/Map Generation/GenerateMap.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateMap : MonoBehaviour
 {
+    public Sprite Default;
+    public Sprite SpawnRoom;
+    public Sprite BossRoom;
+    public Sprite PlantRoom;
+    public Sprite UnexploredRoom;
+    public Sprite CurrRoom;
+
+    void DrawRoomOnMap(Room r){
+        GameObject MapTile = new GameObject("MapTile");
+        Image RoomImage = MapTile.AddComponent<Image>();
+        RoomImage.sprite = s;
+        RectTransform rT = RoomImage.GetComponent<RectTransform>();
+        rT.sizeDelta = new Vector2(Level.Height, Level.Width) * Level.IconScale;
+        rT.position = Location * (Level.IconScale * Level.Height * Level.Scale + (Level.Padding * Level.Height * Level.Scale));
+        RoomImage.transform.SetParent(transform, false);
+    }
     // Start is called before the first frame update
+    void Generate(Room room){
+        if(Random.value > Level.RoomGenerationChance){
+         DrawRoomOnMap(Level.DefaultRoomIcon, Location + new Vector2(-1,0));
+         Generate(Location + new Vector2(-1,0));
+       }
+       //Right
+       if(Random.value > Level.RoomGenerationChance){
+         DrawRoomOnMap(Level.DefaultRoomIcon, Location + new Vector2(1,0));
+         Generate(Location + new Vector2(1,0));
+       }
+       //Up
+       if(Random.value > Level.RoomGenerationChance){
+         DrawRoomOnMap(Level.DefaultRoomIcon, Location + new Vector2(0,1));
+         Generate(Location + new Vector2(0,1));
+       }
+       //Down
+       if(Random.value > Level.RoomGenerationChance){
+         DrawRoomOnMap(Level.UnexploredRoomIcon, new Vector2(0,-1));
+         Generate(Location + new Vector2(0,-1));
+       }
+    }
     void Start()
     {
+        Level.DefaultRoomIcon= Default;
+        Level.BossRoomIcon = BossRoom;
+        Level.SpawnRoomIcon = SpawnRoom;
+        Level.PlantRoomIcon = PlantRoom;
+        Level.UnexploredRoomIcon = UnexploredRoom;
+        Level.CurrentRoomIcon = CurrRoom;
         
+        DrawRoomOnMap(Level.CurrentRoomIcon, new Vector2(0,0));
+
+        Room StartRoom = new Room();
+        StartRoom.Location = new vector2(0,0);
+        StartRoom.RoomImage = Level.CurrentRoom;
+
+        DrawRoomOnMap(StartRoom);
+        //Left
+
+        //right
+
+        //up
+        
+        //down
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
--- Assets/Scripts/Map Generation/Level.cs
+++ Assets/Scripts/Map Generation/Level.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public static class Level
 {
-   public static float _HEIGHT = 500;
-   public static float _WIDTH = 500;
+   public static float Height = 500;
+   public static float Width = 500;
+   public static float Padding = .01f;
+   public static float Scale = 1.0f;
+   public static float IconScale = 0.1f;
+   public static float RoomGenerationChance = 0.5f;
+   public static Sprite BossRoomIcon;
+   public static Sprite SpawnRoomIcon;
+   public static Sprite UnexploredRoomIcon;
+   public static Sprite CurrentRoomIcon;
+   public static Sprite PlantRoomIcon;
+   public static Sprite DefaultRoomIcon;
+   public static List<Room> Rooms = new List<Room>();
+   public static Room CurrentRoom;
 
-   public static float _SCALE = 1.0f;
-   public static Sprite _BossRoomIcon;
-   public static Sprite _SpawnRoomIcon;
-   public static Sprite _UnexploredRoomIcon;
-   public static Sprite _CurrentRoomIcon;
-   public static Sprite _PlantRoomIcon;
-   public static Sprite _DefaultRoomIcon;
+}
 
-
-
+public class Room
+{
+public int RoomNumber = 0;
+public vector2 Location;
+public Sprite RoomImage;
 }
