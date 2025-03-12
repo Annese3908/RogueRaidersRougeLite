e201213efb9668147f14ca6535c6ab420a789356
Roberto Espinoza <bobbye1799@gmail.com>
2025-03-11 23:19:31 -0700

    RoomManagement and EnemySpawner
    In these scripts, they talk to each other but the jist of it is that the enemies will spawn as the player walks in and only three at a time, and 10 enemies in total, and 3 waves in total. Once the room in complete the door will unlock for the player to walk out! Who ever is in charge of the map you might need to make spawn points and attack the colliders to them from the scripts!

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs; // Array for different enemy types
+    public Transform[] spawnPoints;   // Points where enemies will spawn
+    public int enemiesPerWave = 10;   // Total number of enemies per wave
+    public int maxEnemies = 3;        // Maximum number of enemies at a time
+    public float spawnDelay = 2f;     // Delay between spawns
+    public int totalWaves = 3;        // Total number of waves
+
+    private int enemiesSpawned = 0;
+    private int enemiesRemaining;
+    private int wavesCount = 0;
+    private bool isSpawning = false;
+
+    void Start()
+    {
+        enemiesRemaining = enemiesPerWave;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isSpawning && wavesCount < totalWaves)
+        {
+            isSpawning = true;
+            StartCoroutine(SpawnEnemies());
+        }
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (wavesCount < totalWaves)
+        {
+            enemiesRemaining = enemiesPerWave; // Reset enemiesRemaining for the new wave
+            while (enemiesRemaining > 0)
+            {
+                while (enemiesSpawned < maxEnemies && enemiesRemaining > 0)
+                {
+                    SpawnEnemy();
+                    yield return new WaitForSeconds(spawnDelay);
+                }
+                yield return null;
+            }
+            wavesCount++;
+            yield return new WaitForSeconds(spawnDelay); // Delay between waves
+        }
+        // Unlock the room when all waves are finished
+        RoomManager.UnlockRoom();
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemiesRemaining <= 0) return;
+
+        // Determine enemy type based on the current wave
+        GameObject enemyPrefab = GetEnemyTypeForWave();
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        enemiesSpawned++;
+        enemiesRemaining--;
+    }
+
+    GameObject GetEnemyTypeForWave()
+    {
+        if (wavesCount == 0)
+        {
+            return enemyPrefabs[0]; // First wave: only enemy1
+        }
+        else if (wavesCount == 1)
+        {
+            // Second wave: random between enemy1 and enemy2
+            int index = Random.Range(0, 1);
+            return enemyPrefabs[index];
+        }
+        else
+        {
+            // Third wave: random between enemy1, enemy2, and enemy3
+            int index = Random.Range(0, 2);
+            return enemyPrefabs[index];
+        }
+    }
+
+    public void EnemyDefeated()
+    {
+        enemiesSpawned--;
+    }
+}
+
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    // create variables for collider and enemyspawner list
+    public Collider roomCollider;
+    public EnemySpawner[] enemySpawners;
+
+    // create bool variable to see if room is locked, initially start as false
+    private bool roomLocked = false;
+
+    void OnTriggerEntrer(Collider other)
+    {
+        if (other.CompareTag("Player") && !roomLocked)
+        {
+            roomLocked = true;
+            roomCollider.enabled = true;
+
+            //start spawning enemies
+            for (var spawner in enemySpawners)
+            {
+                spawner.StartSpawning();
+            }
+        }
+    }
+
+    public void UnlockRoom()
+    {
+        roomCollider.enabled = false;
+        roomLocked = false;
+    }
+}
