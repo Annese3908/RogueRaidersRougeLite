d5c207e24d2808fda8b064a08031f3ab0ad2e71e
Draggaco <007054509@coyote.csusb.edu>
2025-03-11 23:19:17 -0700

    Added Shop
    - added interactable shop functionality
    - added shop items
    - added healing item
    - added ammo item
    - fixed laggy enemy bug

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -2,11 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour, Interactable
+abstract public class InteractableObject : MonoBehaviour, Interactable
 {
-    //
-    public void Interact()
+    [SerializeField] protected Player player;
+    protected bool isInteractable;
+
+    public abstract void Interact();
+
+    public virtual void Update()
     {
-        Debug.Log("Object Interacted with!");
+        if (isInteractable & Input.GetKeyDown("space"))
+        {
+            Interact();
+        }
+    }
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        //is interactable if player is in trigger
+
+        if (other.GetComponent<Player>() == null) //stops function if collider is not the player
+            return;
+
+        isInteractable = true;
+    }
+
+    protected void OnTriggerExit2D(Collider2D other)
+    {
+        //is not interactable if player leaves trigger
+
+        if (isInteractable)
+        {
+            if (other.GetComponent<Player>() != null)
+                isInteractable = false;
+        }
     }
 }
---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : InteractableObject
+{
+    [SerializeField] protected SpriteRenderer waterSprite;
+
+    protected virtual bool CanBeBought()
+    {
+        return player.FullBucketCount() > 0;
+    }
+
+    protected virtual void BuyItem()
+    {
+        player.SpendBucket();
+    }
+
+    public override void Interact()
+    {
+        if (CanBeBought())
+            BuyItem();
+    }
+
+    public override void Update()
+    {
+        waterSprite.enabled = isInteractable & CanBeBought();
+
+        base.Update();
+    }
+}
